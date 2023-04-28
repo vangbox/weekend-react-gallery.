577b4149fbbb0e9b3f65bdb58d0c7211e3a64e71
Vang Vang <vang2code@gmail.com>
2023-04-28 17:39:41 -0500

    axios.get completed, I was able to pull out the image on the DOM

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,40 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    {   
+        id: 1, 
+        path: 'images/goat_small.jpg', 
+        description: 'Photo of a goat taken at Glacier National Park.', 
+        likes: 0 
+    },
+    {   
+        id: 2, 
+        path: 'images/butterfly.jpg', 
+        description: 'Photo of a Butterfly.', 
+        likes: 0 
+    },
+    {   
+        id: 3, 
+        path: 'images/dosomethinggreat.jpg', 
+        description: 'Photo of a Do Something Great.', 
+        likes: 0 
+    },
+    {   
+        id: 4, 
+        path: 'images/Globe.jpg', 
+        description: 'Photo of a Globe.', 
+        likes: 0 
+    },
+    {   
+        id: 5, 
+        path: 'images/lego.jpg', 
+        description: 'Photo of a cute Lego couple.', 
+        likes: 0 
+    },
+    {   
+        id: 6, 
+        path: 'images/tent.jpg', 
+        description: 'Photo of a wood-house tent.', 
+        likes: 0 
+    }
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,72 @@
 import React from 'react';
 import './App.css';
+//added imports
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+//components import
 
 function App() {
+  //const for app
+  //const for galleryList for the whole list
+  //const for galleryItem for each items
+  const [galleryList, setGalleryList] = useState([]); 
+  const [galleryItem, setGalleryItem] = useState('');
+  
+  // //on load, get galleryList
+  useEffect(() => {
+    getGalleries()
+  }, [])
+
+  // Get axios
+  const getGalleries = () => {
+    axios.get('/gallery')
+    .then(response =>{
+      setGalleryList(response.data)
+      // console.log('this is the response data from axios.GET', response.data); - got the object needed
+    })
+    .catch(err => {
+      alert('error axios.GET!')
+      console.log('error at axios.GET!!');
+    })
+  }
+
+function galleryLists(){
+
+
+}
+function galleryItems(){
+  
+}
+
     return (
+
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {/* galleryList .map below, it should loop to show all gallery with a love it button for each components*/}
+        
+        
+        <table>
+          <thead>
+            <tr>
+              <th>Gallery goes here</th>
+            </tr>
+          </thead>
+          <tbody>
+          {galleryList.map(gallery => (
+              <tr key={gallery.id}>
+                <td><img src={`${gallery.path}`}/></td>
+                <td>{gallery.description}</td>
+                <button>love it!</button>
+                <td>{gallery.likes} people love this!</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        
       </div>
     );
 }
